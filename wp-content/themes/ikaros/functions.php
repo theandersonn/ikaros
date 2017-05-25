d63c25c1b2aea9d9698c82c5659939ece75771b8
theandersonn <theandersonn@gmail.com>
2017-05-24 21:24:41 -0300

    Habilita custom post thumbnail

--- wp-content/themes/ikaros/functions.php
+++ wp-content/themes/ikaros/functions.php
@@ -3,5 +3,11 @@
 // Habilita funcionalidades suportadas
 require get_template_directory() . '/inc/add-theme-suport.php';
 
+// Custom Post Thumbnail
+require get_template_directory() . '/inc/custom-post-thumbnail.php';
+
+// Odin Thumbnail Resizer
+require get_template_directory() . '/classes/odin-thumbnail-resizer.php';
+
 // Carrega Scripts e Estilos
 require get_template_directory() . '/inc/insert-scripts-style.php';
