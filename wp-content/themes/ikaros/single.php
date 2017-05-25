5ff77d5db38a57a8e2178a415a1bb791a2a1d58e
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-05-25 17:42:06 -0300

    Implementa Busca na sidebar single #8

--- wp-content/themes/ikaros/single.php
+++ wp-content/themes/ikaros/single.php
@@ -78,9 +78,9 @@
 			<div class="sidebar-content">
 				<h2 class="title">Search</h2>
 				
-				<form>
+				<form method="get" action="<?php echo home_url(); ?>">
 					<div class="form-group">
-						<input type="search" class="form-page" placeholder="search">
+						<input type="text" name="s" class="form-page" placeholder="search">
 					</div>
 					
 					<button type="submit" class="btn btn-primary">Search</button>
