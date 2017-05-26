5b03cc1a7d9d753e0fb4cbd1653fdda17e64eb62
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-05-26 10:23:57 -0300

    Update na implementação da busca em single #8

---
+++ wp-content/themes/ikaros/searchform.php
@@ -0,0 +1,7 @@
+<form method="get" action="<?php echo home_url(); ?>">
+	<div class="form-group">
+		<input type="text" name="s" class="form-page" placeholder="search">
+	</div>
+	
+	<button type="submit" class="btn btn-primary">Search</button>
+</form>
--- wp-content/themes/ikaros/single.php
+++ wp-content/themes/ikaros/single.php
@@ -86,14 +86,7 @@
 
 			<div class="sidebar-content">
 				<h2 class="title">Search</h2>
-				
-				<form method="get" action="<?php echo home_url(); ?>">
-					<div class="form-group">
-						<input type="text" name="s" class="form-page" placeholder="search">
-					</div>
-					
-					<button type="submit" class="btn btn-primary">Search</button>
-				</form>
+				<?php get_search_form(); ?>
 			</div>
 
 			<hr class="divider-section">
